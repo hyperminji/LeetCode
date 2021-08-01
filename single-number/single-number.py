4bce8f172c84be4565356260b8ef1f5969a1f807
hyperminji <81575572+hyperminji@users.noreply.github.com>
2021-08-01 14:23:28 +0900

    Time: 128 ms (85.26%), Space: 16.6 MB (60.18%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,15 +1,25 @@
 #pop -> append 
 #중복이 되면 remove
 #남은 것 return 
+
+
+# time: O(n²), space : O(n)
+#class Solution:
+#    def singleNumber(self, nums: List[int]) -> int:
+#        nums_list = []
+#        for i in nums:
+#            if i not in nums_list:
+#                nums_list.append(i)
+#            else:
+#                nums_list.remove(i)
+#        result= nums_list.pop()
+        
+#        return result
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums_list = []
+        answer = 0
         for i in nums:
-            if i not in nums_list:
-                nums_list.append(i)
-            else:
-                nums_list.remove(i)
-        result= nums_list.pop()
-        
-        return result
+            answer = answer^i
+        return answer
         
