94549b57745b1b93d250a3531b9c4887085d6c7e
hyperminji <81575572+hyperminji@users.noreply.github.com>
2021-08-30 00:54:14 +0900

    Time: 240 ms (63.42%), Space: 21.1 MB (81.13%) - LeetHub

---
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        answer = [0]*length
+        
+        answer[0] = 1
+        for i in range(1, length):
+            answer[i] = nums[i -1]*answer[i-1]
+        right=1
+        for i in reversed(range(length)):
+            answer[i] = answer[i]*right
+            right= right*nums[i]
+        return answer
+        
