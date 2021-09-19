146fa9e19f8cab024b0c2d8ec62330a463330256
hyperminji <81575572+hyperminji@users.noreply.github.com>
2021-09-20 01:21:59 +0900

    Time: 168 ms (66.56%), Space: 15.7 MB (10.97%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,5 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        answer = nums[len(nums)//2]
+#첫번째 풀이
+#        nums.sort()
+#        answer = nums[len(nums)//2]
+#        return answer
+#두번째 풀이
+        count = 0
+        answer = None
+        for n in nums:
+            if count == 0:
+                answer = n
+            if n == answer:
+                count = count +1
+            else:
+                count = count -1
         return answer
