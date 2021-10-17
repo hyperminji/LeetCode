cc2c9c4d203608e7d173106edf33ef569c26f653
hyperminji <81575572+hyperminji@users.noreply.github.com>
2021-10-18 00:08:35 +0900

    Time: 24 ms (96.58%), Space: 14.2 MB (46.67%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.py
+++ unique-binary-search-trees/unique-binary-search-trees.py
@@ -7,4 +7,17 @@ class Solution:
             for j in range(i):
                 tree[i] += tree[j] * tree[i-1-j]
         return tree[n]
-        
+
+
+#class Solution:
+#    def numTrees(self, n: int) -> int:
+#        
+#        if n==0 or n ==1:
+#            return 1
+#        trees = [0]*(n+1)
+#        for i in range(n):
+#            left_tree = self.numTrees(i)
+#            right_tree = self.numTrees(n-i-1)
+#            trees = trees + left_tree*right_tree
+#        return trees
+#        //  Time Limit Exceeded
