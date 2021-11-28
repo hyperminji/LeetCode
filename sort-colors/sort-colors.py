14465300d23af4e3ad84e0a8a7a5c5502aa42e4a
hyperminji <81575572+hyperminji@users.noreply.github.com>
2021-11-28 22:39:55 +0900

    Time: 36 ms (48.21%), Space: 14.3 MB (46.96%) - LeetHub

---
+++ sort-colors/sort-colors.py
@@ -0,0 +1,25 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        point0 = 0
+        point2 = len(nums) -1
+        
+        i = 0
+        count = 0 # 정렬완료된 원소
+        
+        while count < len(nums):
+            if nums[i] == 0:  # 0 이라면 point0자리로 이동
+                nums[i], nums[point0] = nums[point0], nums[i]
+                point0 = point0+1
+                i = i +1
+            elif nums[i] ==1:  #1자리에 제대로 있으므로 pass
+                i = i+1
+            else: #2일경우 point2로 이동
+                nums[i], nums[point2] = nums[point2], nums[i]
+                point2= point2-1
+            count = count+1
+        
+                
+        
