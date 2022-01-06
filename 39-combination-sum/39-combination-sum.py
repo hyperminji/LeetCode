c43d2619445d7610248e4c3b89a8297c782da2c3
hyperminji <81575572+hyperminji@users.noreply.github.com>
2022-01-06 23:53:42 +0900

    Time: 72 ms (78.73%), Space: 14.3 MB (78.29%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        answer = []
+        def finder(candidates, target, t):
+            if not target:
+                answer.append(t)
+                return 
+            for i, num in enumerate(candidates):
+                if target >= num:
+                    finder(candidates[i:], target - num, t + [num])
+                else:
+                    break
+        finder(candidates, target, [])
+        return answer
