28f78e0a6166e7148f19bdab70c2b46ca8c363e0
hyperminji <81575572+hyperminji@users.noreply.github.com>
2022-01-07 00:01:31 +0900

    Time: 124 ms (24.75%), Space: 14.2 MB (78.29%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,5 +1,26 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        
+        def dfs(cansum, index, path):
+            #음수이거나 0인 경우 종료
+            if cansum < 0:
+                return
+            if cansum == 0: #종료 조건을 만족할 수 없기 때문에 무한탐색이 되어버린다
+                result.append(path)
+                return
+            
+            #재귀
+            for i in range(index, len(candidates)):
+                dfs(cansum-candidates[i],i, path+[candidates[i]])
+        
+        dfs(target, 0, [])
+        return result
+                
+        
+'''
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         answer = []
         def finder(candidates, target, t):
@@ -13,3 +34,5 @@ class Solution:
                     break
         finder(candidates, target, [])
         return answer
+'''
+        
