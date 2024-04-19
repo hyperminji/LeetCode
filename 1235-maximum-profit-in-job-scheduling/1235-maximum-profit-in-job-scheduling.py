346d0bb13db20cd0100b0a0346d6d4adf0432ea5
hyperminji <81575572+hyperminji@users.noreply.github.com>
2024-04-19 23:27:40 +0900

    Time: 660 ms (65.45%), Space: 31.6 MB (60.94%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        n = len(startTime)
+        Job = namedtuple('Job', ('start','end','profit'))
+        jobs = sorted([Job(startTime[i], endTime[i], profit[i]) for i in range(n)])
+
+        nextPossJob = [n]*n
+        
+        startorder_i = 0
+        for endorder_i in sorted(range(n), key=lambda x:jobs[x].end):
+            while startorder_i < n and jobs[endorder_i].end > jobs[startorder_i].start:
+                startorder_i += 1
+            if startorder_i == n:
+                break
+            nextPossJob[endorder_i] = startorder_i
+
+        dp = [0]*(n+1)
+        
+        for i in range(n-1, -1, -1):
+            dp[i] = max(jobs[i].profit + dp[nextPossJob[i]], dp[i+1])
+        
+        return dp[0]  
